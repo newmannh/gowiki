9fb316dc446467652a05bfda73b08f2a6e00396d
Nathan <newmannh@gmail.com>
2014-07-31 09:31:03 -0400

    last touches

--- wiki.go
+++ wiki.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"flag"
 )
 
 /////Part 1
@@ -97,9 +98,57 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	return m[2], nil
 }
 
-func main() {
+func mainTwo() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+//// Part three
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return fun(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w,r,m[2])
+	}
+}
+
+func viewHandlerFunc(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandlerFunc(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title, Body: []byte(err.Error())}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandlerFunc(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err = p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	flag.Parse()
+	http.HandlerFunc("/view/",makeHandler(viewHandlerFunc))
+	http.HandlerFunc("/edit/",makeHandler(editHandlerFunc))
+	http.HandlerFunc("/save/",makeHandler(saveHandlerFunc))
+
 }
